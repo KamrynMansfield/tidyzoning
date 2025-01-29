d276008c513f8e99720f7a4f536186b32391b7f4
Kamryn Mansfield <kamryn_mansfield@gsd.harvard.edu>
2025-01-29 06:34:17 -0500

    started get_raw_zoning_req

---
+++ R/get_raw_zoning_req.R
@@ -0,0 +1,6 @@
+get_raw_zoning_req <- function(tidyzoning){
+  # ozfs_variable is a data frame I will probably use
+  # it has each variable along with the full name of that variable
+  # I would need to update it every time there is a new variable
+
+}
