2215a92e2c4d67b3f9c6f2f7dba9706d3063ae18
Kamryn Mansfield <kamryn_mansfield@gsd.harvard.edu>
2025-04-07 17:21:47 -0400

    updated check_fl_area

--- R/check_fl_area.R
+++ R/check_fl_area.R
@@ -2,67 +2,144 @@
 #'
 #' `check_fl_area()` takes a tidybuilding and a tidydistrict to see if the district's zoning code allows the tidybuilding based on floor area.
 #'
-#' @inheritParams add_setbacks
+#' @inheritParams check_height
 #'
 #' @return
-#' Returns TRUE or FALSE stating whether or not the building would be allowed in the district based on floor area.
+#' Returns TRUE, FALSE, or MAYBE stating whether or not the building would be allowed in the district based on floor area.
 #' @export
 #'
-check_fl_area <- function(tidybuilding, tidydistrict){
-  structure_constraints <- fromJSON(tidydistrict$structure_constraints)
+check_fl_area <- function(tidybuilding, tidydistrict = NULL, tidyparcel = NULL, zoning_req = NULL){
+  # if zoning_req is not given, we need to run the get_zoning_req function
+  if (is.null(zoning_req)){
+    zoning_req <- get_zoning_req(tidybuilding, tidydistrict, tidyparcel)
+  }
 
-  if (length(tidybuilding$floor_area) == 1){
-    fl_area <- tidybuilding$floor_area[[1]]
-  } else if (length(tidybuilding$total_floors) == 1){
-    floors <- tidybuilding$total_floors[[1]]
-    fl_area <- st_area(tidybuilding) * floors
+  # if the zonning_req is "character" and not "data.frame", there were no zoning requirements recorded.
+  # we return maybe with a warning
+  if (class(zoning_req) == "character"){
+    return("MAYBE")
+    warning("No zoning requirements recorded for this district")
+  }
+
+  # establish the constaint we are looking at
+  constraint <- "fl_area"
+
+  # getting the value from the building's attributes
+  # if the fl_area is not recorded
+  if (length(tidybuilding$bldg_info$gross_fl_area) == 1){
+    value <- tidybuilding$bldg_info$gross_fl_area[[1]]
+  } else if (length(tidybuilding$unit_info$fl_area) == 1){
+    value <- sum(tidybuilding$unit_info$fl_area * tidybuilding$unit_info$qty)
+    warning("gross_fl_area calculated by summing unit fl_areas")
   } else{
     return(FALSE)
     warning("No floor area found in tidybuilding")
   }
 
-  zoning_req <- get_zoning_req(tidybuilding, tidydistrict)
 
-  if (class(zoning_req) == "character"){
-    return(TRUE)
-    warning("No zoning requirements recorded for this district")
-  }
+  # assume min and max values if they are not recorded
+  # if specific constraint we are looking for is not in zoning requirements, we assume any value is allowed
+  if (constraint %in% zoning_req$constraint_name){
+    min_requirement <- zoning_req[zoning_req$constraint_name == constraint, "min_value"][[1]]
+    max_requirement <- zoning_req[zoning_req$constraint_name == constraint, "max_value"][[1]]
 
-  if ("fl_area" %in% zoning_req$constraint_name){
-    min_fl_area <- zoning_req[zoning_req$constraint_name == "fl_area", "min_value"]
-    max_fl_area <- zoning_req[zoning_req$constraint_name == "fl_area", "max_value"]
-
-    if (is.na(min_fl_area)){
-      min_fl_area <- 0
+    if (is.na(min_requirement[[1]])){
+      min_requirement <- 0
     }
 
-    if (is.na(max_fl_area)){
-      max_fl_area <- 1000000
+    if (is.na(max_requirement[[1]])){
+      max_requirement <- 1000
     }
-
-    # change fl_area units to match the ones recorded in the code
-    if (!is.na(zoning_req[zoning_req$constraint_name == "fl_area", "units"])){
-      fl_area <- set_units(fl_area, "ft2")
-      fl_area_units <- zoning_req[zoning_req$constraint_name == "fl_area", "units"]
-
-      if (fl_area_units == "square feet"){
-        fl_area_units <- "ft2"
-      } else if (fl_area_units == "square meters"){
-        fl_area_units <- "m2"
-      } else if (fl_area_units == "acres"){
-        fl_area_units <- "acre"
-      }
-
-      units(max_fl_area) <- fl_area_units
-      units(min_fl_area) <- fl_area_units
-      max_fl_area <- set_units(max_fl_area, "ft2")
-      max_fl_area <- set_units(max_fl_area, "ft2")
-    }
-
-    return(fl_area >= min_fl_area & fl_area <= max_fl_area)
 
   } else{
     return(TRUE)
   }
+
+  # this tests for the usual case when each min and max requirements have one value
+  if (length(min_requirement) & length(max_requirement) == 1){
+    return(value >= min_requirement & value <= max_requirement)
+  }
+
+  # this is where multiple values are listed for a unique zoning requirement
+  # assign 2 minimum values
+  min_check_1 <- min(min_requirement) <= value
+  min_check_2 <- max(min_requirement) <= value
+  if (is.null(zoning_req[zoning_req$constraint_name == constraint, "min_val_note"][[1]])){
+    min_val_either <- FALSE
+  } else if(is.na(zoning_req[zoning_req$constraint_name == constraint, "min_val_note"][[1]])){
+    min_val_either <- FALSE
+  } else{
+    min_val_either <- zoning_req[zoning_req$constraint_name == constraint, "min_val_note"][[1]] == "either"
+  }
+
+  # see if the minimum values are met
+  if (min_val_either == TRUE){
+    if (min_check_1 == FALSE & min_check_2 == FALSE){
+      min_check <- FALSE
+    } else{
+      min_check <- TRUE
+    }
+
+  } else{
+    if (min_check_1 == TRUE & min_check_2 == TRUE){
+      min_check <- TRUE
+    } else if (min_check_1 == FALSE & min_check_2 == FALSE){
+      min_check <- FALSE
+    } else{
+      min_check <- "MAYBE"
+    }
+  }
+
+  # assign 2 maximum values
+  max_check_1 <- min(max_requirement) >= value
+  max_check_2 <- max(max_requirement) >= value
+  if (is.null(zoning_req[zoning_req$constraint_name == constraint, "max_val_note"][[1]])){
+    max_val_either <- FALSE
+  } else if(is.na(zoning_req[zoning_req$constraint_name == constraint, "max_val_note"][[1]])){
+    max_val_either <- FALSE
+  } else{
+    max_val_either <- zoning_req[zoning_req$constraint_name == constraint, "max_val_note"][[1]] == "either"
+  }
+
+  # see if the maximum values are met
+  if (max_val_either == TRUE){
+    if (max_check_1 == FALSE & max_check_2 == FALSE){
+      max_check <- FALSE
+    } else{
+      max_check <- TRUE
+    }
+
+  } else{
+    if (max_check_1 == TRUE & max_check_2 == TRUE){
+      max_check <- TRUE
+    } else if (max_check_1 == FALSE & max_check_2 == FALSE){
+      max_check <- FALSE
+    } else{
+      max_check <- "MAYBE"
+    }
+  }
+
+  # use both min and max value check to see if it is allowed
+  if (max_check == FALSE | min_check == FALSE){
+    return(FALSE)
+  } else if (max_check == TRUE & min_check == TRUE){
+    return(TRUE)
+  } else{
+    explanation <- c()
+    if (!is.na(zoning_req[zoning_req$constraint_name == constraint, "min_val_note"][[1]])){
+      explanation <- c(explanation,zoning_req[zoning_req$constraint_name == constraint,"min_val_note"][[1]])
+    }
+    if (!is.na(zoning_req[zoning_req$constraint_name == constraint, "max_val_note"][[1]])){
+      explanation <- c(explanation,zoning_req[zoning_req$constraint_name == constraint,"max_val_note"][[1]])
+    }
+
+    if (length(explanation) > 0){
+      warning(explanation)
+    }
+
+    return("MAYBE")
+
+  }
+
 
 }
