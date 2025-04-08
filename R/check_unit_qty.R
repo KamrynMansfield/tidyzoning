aa7a8a161e90d28918f49538de00a19508fb7e40
Kamryn Mansfield <kamryn_mansfield@gsd.harvard.edu>
2025-04-08 19:36:54 -0400

    started check_unit_qty function

---
+++ R/check_unit_qty.R
@@ -0,0 +1,15 @@
+# check_unit_qty
+# zoning_req: see if there is a unit_qty or pct_units_0bed(1bed, 2bed, 3bed, 4bed) in the requirements
+# tidybuilding: calculate the different values
+#
+# do a loop to perform the check for each variable
+#
+# it all are TRUE, then TRUE
+# if all are FALSE, then FALSE
+# else, MAYBE
+#
+# The question is how do we get the warnings for the maybe, but it shouldn't be too bad.
+
+check_unit_qty <- function(tidybuilding, tidydistrict = NULL, tidyparcel = NULL, zoning_req = NULL){
+
+}
