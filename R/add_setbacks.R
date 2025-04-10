71dd7e5be78f70a31966b43b8ba856329efcf322
Kamryn Mansfield <kamryn_mansfield@gsd.harvard.edu>
2025-04-10 15:54:04 -0400

    add_setbacks update

--- R/add_setbacks.R
+++ R/add_setbacks.R
@@ -11,11 +11,14 @@
 #' @export
 #'
 
-
-add_setbacks <- function(tidyparcel, tidydistrict, tidybuilding){
+add_setbacks <- function(tidyparcel, tidydistrict, tidybuilding, zoning_req = NULL){
   tidyparcel <- tidyparcel[tidyparcel$side != "centroid",]
   tidyparcel <- tidyparcel[!is.na(tidyparcel$side),]
-  zoning_req <- get_zoning_req(tidybuilding, tidydistrict, tidyparcel)
+
+  # if zoning_req is not given, we need to run the get_zoning_req function
+  if (is.null(zoning_req)){
+    zoning_req <- get_zoning_req(tidybuilding, tidydistrict, tidyparcel)
+  }
 
   if (class(zoning_req) == "character"){
     tidyparcel$setback <- NA
@@ -27,19 +30,26 @@ add_setbacks <- function(tidyparcel, tidydistrict, tidybuilding){
                 `Interior side` = "setback_side_int",
                 `Exterior side` = "setback_side_ext",
                 rear = "setback_rear")
+
+  # loop through each side
+  setback_value <- list()
+  units_value <- c()
   for (i in 1:nrow(tidyparcel)){
     side_type <- tidyparcel[[i,"side"]]
     filtered_constraints <- zoning_req |>
       filter(constraint_name == name_key[[side_type]])
 
     if (nrow(filtered_constraints) > 0){
-      tidyparcel[i,"setback"] <- filtered_constraints[1,"min_value"]
-      tidyparcel[i,"units"] <- filtered_constraints[1,"units"]
+      setback_value[i] <- filtered_constraints[1,"min_value"]
+      units_value <- c(units_value, filtered_constraints[1,"unit"])
     } else {
-      tidyparcel[i,"setback"] <- NA
-      tidyparcel[i,"units"] <- NA
+      setback_value[i] <- NA
+      units_value <- c(units_value, NA)
     }
   }
+
+  tidyparcel$setback <- I(setback_value)
+  tidyparcel$units <- units_value
 
   tidyparcel
 }
