39b641a40d2d16e4f1e49c096ed26f6da29b54b2
Kamryn Mansfield <kamryn_mansfield@gsd.harvard.edu>
2025-04-10 11:13:28 -0400

    add_setbacks updates

--- R/add_setbacks.R
+++ R/add_setbacks.R
@@ -33,9 +33,7 @@ add_setbacks <- function(tidyparcel, tidydistrict, tidybuilding){
       filter(constraint_name == name_key[[side_type]])
 
     if (nrow(filtered_constraints) > 0){
-      setback_value <- filtered_constraints[1,"min_value"]
-
-      tidyparcel[i,"setback"] <- setback_value
+      tidyparcel[i,"setback"] <- filtered_constraints[1,"min_value"]
       tidyparcel[i,"units"] <- filtered_constraints[1,"units"]
     } else {
       tidyparcel[i,"setback"] <- NA
@@ -45,42 +43,3 @@ add_setbacks <- function(tidyparcel, tidydistrict, tidybuilding){
 
   tidyparcel
 }
-
-#
-# parcel_ids <- unique(tidyparcel_for_testing$parcel_id)
-# district_idx <- c()
-# for (i in 1:length(parcel_ids)){
-#   parcel <- tidyparcel_for_testing |>
-#     filter(parcel_id == parcel_ids[[i]])
-#   district_idx <- c(district_idx, find_district_idx(parcel, tidyzoning_for_testing))
-#
-# }
-#
-# tidyparcel <- tidyparcel_for_testing |>
-#   filter(parcel_id == parcel_ids[[3]]) |>
-#   filter(side != "centroid")
-#
-# tidydistrict <- tidyzoning_for_testing[district_idx[[2]],]
-#
-# tidybuilding <- tidybuilding_ex
-#
-# tidyparcel_for_testing |>
-#   filter(parcel_id == parcel_ids[[2]]) |>
-#   ggplot() +
-#   geom_sf(aes(color = side))
-#
-#
-# land_use_check <- c()
-# for (i in 1:length(parcel_ids)){
-#   district <- tidyzoning_for_testing[district_idx[[i]],]
-#   land_use_check <- c(land_use_check, check_land_use(tidybuilding, district))
-# }
-#
-# parcel_with_setbacks <- add_setbacks(tidyparcel, tidydistrict, tidybuilding)
-#
-# buildable_area <- get_buildable_area(parcel_with_setbacks)
-#
-# show_shapes(tidyparcel, buildable_area)
-#
-# check_footprint(tidybuilding,buildable_area)
-
