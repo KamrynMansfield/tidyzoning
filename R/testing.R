7b27c7beb9349064aa99a490a0a73fa0d6b4c1e9
Kamryn Mansfield <kamryn_mansfield@gsd.harvard.edu>
2025-05-20 13:25:07 -0400

    small changes to _pkgdown

--- R/testing.R
+++ R/testing.R
@@ -1,16 +1,16 @@
-
-parcels_path <- "inst/extdata/Cockrell_Hill_parcels.geojson"
-zoning_path <- "inst/extdata/Cockrell_Hill_zoning.geojson"
-building_path <- "inst/extdata/bldg_12_fam.json"
-zoning_old <- sf::st_read("../personal_rpoj/FortWorthData/nza_to_ozfs_all_counties/ozfs/Cockrell Hill.geojson")
-zoning <- sf::st_read(zoning_path)
-
-
-tbldg <- unify_tidybuilding(building_path)
-
-tidyparcel_dims <- get_tidyparcel_dim(parcels_path)
-tidyparcel_geo <- get_tidyparcel_geo(parcels_path)
-
-check_land_use(tbldg,zoning_old[1,])
-get_zoning_req(tbldg, zoning_old[4,],tidyparcel_dims)
-
+#
+# parcels_path <- "inst/extdata/Cockrell_Hill_parcels.geojson"
+# zoning_path <- "inst/extdata/Cockrell_Hill_zoning.geojson"
+# building_path <- "inst/extdata/bldg_12_fam.json"
+# zoning_old <- sf::st_read("../personal_rpoj/FortWorthData/nza_to_ozfs_all_counties/ozfs/Cockrell Hill.geojson")
+# zoning <- sf::st_read(zoning_path)
+#
+#
+# tbldg <- unify_tidybuilding(building_path)
+#
+# tidyparcel_dims <- get_tidyparcel_dim(parcels_path)
+# tidyparcel_geo <- get_tidyparcel_geo(parcels_path)
+#
+# check_land_use(tbldg,zoning_old[1,])
+# get_zoning_req(tbldg, zoning_old[4,],tidyparcel_dims)
+#
