385e9391db41363c3ea50718bc9a2ff4e00bdc49
Kamryn Mansfield <kamryn_mansfield@gsd.harvard.edu>
2025-05-21 13:02:12 -0400

    created find_dist_idx

--- R/find_district_idx.R
+++ R/find_district_idx.R
@@ -1,18 +1,26 @@
 #' Find corresponding tidydistrict
 #'
 #' @description
-#' `find_district_idx()` takes in a tidyzoning object and a tidyparcel object.
-#' Returns the row number of the district to which the parcel belongs.
+#' `find_district_idx()` adds a zoning_id column to the parcels data that
+#' contains the tidyzoning row index of the district the parcel is in
 #'
-#' @param tidyparcel_geo A tidyparcel object is a simple features object depicting each side of a parcel and its label (front, Interior side, Exterior side, rear, centroid).
-#' @param tidyzoning The tidyzoning object is a simple features object with a row for each zoning district and columns holding the geojson formatted zoning requirements of each district.
+#' @param parcels_centroids_sf An sf object with the parcel centroids
+#' @param tidyzoning A simple features object with a row for each zoning district and columns holding the geojson formatted zoning requirements of each district.
 #'
-#' @return The row number of the tidyzoning object that contains the disrict to which the parcel belongs.
-#' Returns NA if there is no district found.
+#' @return The same parcels centroids object that was input but with an additional
+#' column stating the row number of the corresponding tidyzoning district
 #' @export
 #'
-find_district_idx <- function(tidyparcel_geo, tidyzoning){
+find_district_idx <- function(parcels_centroids_sf, tidyzoning){
 
-# create a function that will get the index of the district the parcel is in
+  tidyzoning$zoning_id <- 1:nrow(tidyzoning)
+
+  parcels_with_zoning_id <- sf::st_join(parcels_centroids_sf, tidyzoning["zoning_id"])
+
+  # in the future, I may have to account for the rare case where
+  # there is a parcel in two base districts
+
+
+  return(parcels_with_zoning_id)
 
 }
