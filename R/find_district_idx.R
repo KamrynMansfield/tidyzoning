e5470a3eda640b2a5fdd5ecf06ec2c7f3fce00d5
Kamryn Mansfield <kamryn_mansfield@gsd.harvard.edu>
2025-05-28 15:05:01 -0400

    st_make_valid to find_district_index

--- R/find_district_idx.R
+++ R/find_district_idx.R
@@ -14,9 +14,10 @@
 #'
 find_district_idx <- function(parcels_centroids_sf, tidyzoning, idx_col_name = "zoning_id"){
 
+  tidyzoning <- sf::st_make_valid(tidyzoning)
   tidyzoning[[idx_col_name]] <- 1:nrow(tidyzoning)
 
-  parcels_with_zoning_id <- sf::st_join(parcels_centroids_sf, tidyzoning[idx_col_name])
+  parcels_with_zoning_id <- sf::st_join(sf::st_make_valid(parcels_centroids_sf), tidyzoning[idx_col_name])
 
   # in the future, I may have to account for the rare case where
   # there is a parcel in two base districts
